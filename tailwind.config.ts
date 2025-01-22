f90877aca954ea05a3f20d7d6d62db153fa243af
fs <faruxue2019@163.com>
2025-01-22 15:21:00 +0800

    feat: 字体

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,10 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        styleFont: [
-          '"LXGW Neo ZhiSong"',
-          "sans-serif",
-        ],
+        styleFont: ['"LXGW Neo ZhiSong"', "sans-serif"],
       },
       colors: {
         background: "hsl(var(--background))",
