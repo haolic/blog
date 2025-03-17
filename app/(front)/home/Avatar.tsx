6729db9e8dabd66289d6f09eacc9c181422b7cde
fs <faruxue2019@163.com>
2025-03-17 16:17:24 +0800

    feat: Update Avatar component and test page with new layout and image handling

--- app/(front)/home/Avatar.tsx
+++ app/(front)/home/Avatar.tsx
@@ -15,7 +15,7 @@ export default function Avatar() {
         drag
         dragConstraints={constraintsRef}
         dragElastic={0.2}
-        className="relative rounded-full overflow-hidden w-[200px] h-[200px] cursor-grab active:cursor-grabbing"
+        className="relative rounded-full w-[200px] h-[200px] cursor-grab active:cursor-grabbing"
         animate={{
           x: 100,
           y: 100,
@@ -29,13 +29,38 @@ export default function Avatar() {
           ease: "backOut",
         }}
       >
-        <Image
-          src="/avatar.jpg"
-          alt="avatar"
-          width={200}
-          height={200}
-          className="pointer-events-none"
-        />
+        <div className="rounded-full flex pt-6">
+          <div className="w-[200px] h-[200px] border rounded-full absolute"></div>
+          <div className="w-[200px] h-[200px] rounded-full relative box-content m-auto group">
+            <div className="w-[200px] h-[200px] rounded-full overflow-hidden">
+              <Image
+                src="/avatar.jpg"
+                alt="avatar"
+                width={200}
+                height={200}
+                className="pointer-events-none"
+              />
+            </div>
+            <div className="w-[200px] h-[156px] absolute top-0 left-0 pt-11 -translate-y-11 overflow-hidden">
+              <Image
+                src="/avatar-bg-transparent.png"
+                alt="bg"
+                width={200}
+                height={200}
+                className="transition-all scale-97 group-hover:scale-125 pointer-events-none"
+              />
+            </div>
+            <div className="w-[200px] h-[100px] absolute bottom-0 left-0 rounded-bl-full rounded-br-full overflow-hidden">
+              <Image
+                src="/avatar-bg-transparent.png"
+                alt="bg"
+                width={200}
+                height={200}
+                className="-translate-y-1/2 transition-all scale-97 group-hover:scale-125 pointer-events-none"
+              />
+            </div>
+          </div>
+        </div>
       </motion.div>
     </motion.div>
   );
--- app/(front)/test/page.tsx
+++ app/(front)/test/page.tsx
@@ -4,24 +4,33 @@ const Test = () => {
   return (
     <div>
       <div className="rounded-full flex pt-6">
-        <div className="w-56 h-56 border rounded-full absolute"></div>
-        <div className="w-56 h-56 rounded-full relative box-content m-auto group">
-          <div className="w-56 h-[156px] absolute top-0 left-0 pt-11 -translate-y-11 overflow-hidden">
+        <div className="w-[200px] h-[200px] border rounded-full absolute"></div>
+        <div className="w-[200px] h-[200px] rounded-full relative box-content m-auto group">
+          <div className="w-[200px] h-[200px] rounded-full overflow-hidden">
             <Image
-              src="/avatar-bg-transparent.png"
-              alt="bg"
-              width={224}
-              height={224}
-              className="transition-all scale-97 group-hover:scale-125"
+              src="/avatar.jpg"
+              alt="avatar"
+              width={200}
+              height={200}
+              className="pointer-events-none"
             />
           </div>
-          <div className="w-56 h-28 absolute bottom-0 left-0 rounded-bl-full rounded-br-full overflow-hidden">
+          <div className="w-[200px] h-[156px] absolute top-0 left-0 pt-11 -translate-y-11 overflow-hidden">
             <Image
               src="/avatar-bg-transparent.png"
               alt="bg"
-              width={224}
-              height={224}
-              className="-translate-y-1/2 transition-all scale-97 group-hover:scale-125"
+              width={200}
+              height={200}
+              className="transition-all scale-97 group-hover:scale-125 pointer-events-none"
+            />
+          </div>
+          <div className="w-[200px] h-[100px] absolute bottom-0 left-0 rounded-bl-full rounded-br-full overflow-hidden">
+            <Image
+              src="/avatar-bg-transparent.png"
+              alt="bg"
+              width={200}
+              height={200}
+              className="-translate-y-1/2 transition-all scale-97 group-hover:scale-125 pointer-events-none"
             />
           </div>
         </div>
