a9829c3d4a68038be1ec9f1be971628669bd4531
fs <faruxue2019@163.com>
2025-04-22 10:39:09 +0800

    feat: 统一代码风格，使用单引号并添加.prettierrc配置文件

--- app/(front)/home/AvatarAnimation.tsx
+++ app/(front)/home/AvatarAnimation.tsx
@@ -1,8 +1,10 @@
-import { cn } from "@/lib/utils";
-import AvatarImg from "./AvatarImg";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useCallback, useEffect, useRef } from "react";
+'use client';
+
+import { cn } from '@/lib/utils';
+import AvatarImg from './AvatarImg';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useCallback, useEffect, useRef } from 'react';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -22,9 +24,9 @@ const AvatarAnimation = ({ className }: { className?: string }) => {
     // 创建动画时间线
     const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: "body", // 使用整个页面作为触发器
-        start: "top+=200 top", // 当页面顶部向下滚动200px时开始
-        end: "bottom bottom", // 当页面底部到达视口底部时结束
+        trigger: 'body', // 使用整个页面作为触发器
+        start: 'top+=200 top', // 当页面顶部向下滚动200px时开始
+        end: 'bottom bottom', // 当页面底部到达视口底部时结束
         scrub: 1, // 平滑跟随滚动，值越大越平滑
         markers: false, // 查看触发点（调试用）
         invalidateOnRefresh: true, // 刷新时重新计算触发器
@@ -35,12 +37,12 @@ const AvatarAnimation = ({ className }: { className?: string }) => {
     tl.to(avatarRef.current, {
       x: -(windowWidth / 2 - width * 0.5 - 2), // 先向右移动到窗口右侧
       duration: 0.3, // 占总滚动的前半部分
-      ease: "power1.inOut",
+      ease: 'power1.inOut',
     }).to(avatarRef.current, {
       x: -(windowWidth / 2 - width * 0.5 - 2), // 保持水平位置
       y: windowHeight - 38, // 向下移动到页面底部
       duration: 0.7, // 占总滚动的后半部分
-      ease: "power1.inOut",
+      ease: 'power1.inOut',
     });
 
     return () => {
@@ -61,7 +63,7 @@ const AvatarAnimation = ({ className }: { className?: string }) => {
     <div
       ref={avatarRef}
       className={cn(
-        "fixed top-[4px] left-1/2 -translate-x-1/2 z-20",
+        'fixed top-[4px] left-1/2 -translate-x-1/2 z-20',
         className
       )}
     >
